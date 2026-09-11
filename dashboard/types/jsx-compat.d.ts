1d27b4644d0e689d69aa393c8afa20b355c0d6ce
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-09-11 23:03:02 +0530

    Allow boolean refs in landing page JSX typing

--- dashboard/types/jsx-compat.d.ts
+++ dashboard/types/jsx-compat.d.ts
@@ -1,10 +1,14 @@
 import type React from "react";
 
 declare module "react" {
+  interface RefAttributes<T> {
+    ref?: React.Ref<T> | boolean;
+  }
+
   namespace JSX {
     interface IntrinsicAttributes {
       children?: React.ReactNode;
-      ref?: React.Ref<any>;
+      ref?: React.Ref<any> | boolean;
       className?: string;
       style?: React.CSSProperties;
     }
