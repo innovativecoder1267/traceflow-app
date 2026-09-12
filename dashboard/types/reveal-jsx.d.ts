66ff3a6897e487b0f88832f885c8cf8109a7ef53
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-09-12 13:15:34 +0530

    Fix dynamic Reveal element JSX typing

---
+++ dashboard/types/reveal-jsx.d.ts
@@ -0,0 +1,14 @@
+import type React from "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      [elemName: string]: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
+export {};
