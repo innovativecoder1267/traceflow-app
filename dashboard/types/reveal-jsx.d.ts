9aa92dab5545dde64a3aa0d965bd1f97d151daea
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-09-12 13:17:37 +0530

    Fix Reveal JSX prop typing

--- dashboard/types/reveal-jsx.d.ts
+++ dashboard/types/reveal-jsx.d.ts
@@ -2,6 +2,10 @@ import type React from "react";
 
 declare module "react" {
   namespace JSX {
+    interface IntrinsicAttributes {
+      [key: string]: any;
+    }
+
     interface IntrinsicElements {
       [elemName: string]: React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement>,
